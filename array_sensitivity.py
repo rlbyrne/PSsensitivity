fix normalization

array_sensitivity.py
@@ -11,7 +11,6 @@ c = 3e8
 def get_cosmological_parameters():
 
     cosmological_parameter_dict = {
-        "D_H": 3000,  # Hubble distance, units Mpc/h
         "HI rest frame wavelength": 0.21,  # 21 cm wavelength, units m
         "omega_M": 0.27,  # Matter density
         "omega_k": 0,  # Curvature
@@ -19,12 +18,15 @@ def get_cosmological_parameters():
         "omega_B": 0.04,  # Baryon density
         "mass_frac_HI": 0.015,  # Mass fraction of neutral hydrogen
         "bias": 0.75,  # Bias between matter PS and HI PS
-        "h": 0.71,
+        "h": 0.71,  # Dimensionless Hubble constant
     }
     # Derived quantities
     cosmological_parameter_dict["H_0"] = (
-        c / cosmological_parameter_dict["D_H"]
-    )  # Hubble constant, units h*s/(m Mpc)
+        cosmological_parameter_dict["h"] * 1.0e5
+    )  # Hubble constant, units m / s / Mpc
+    cosmological_parameter_dict["D_H"] = (
+        c / cosmological_parameter_dict["H_0"]
+    )  # Hubble distance, units Mpc
 
     return cosmological_parameter_dict
 
@@ -87,11 +89,10 @@ def get_visibility_stddev(
 
 
 def get_kpar_conversion_factor(avg_freq_hz):
-    # See "Cosmological Parameters and Conversions Memo"
 
     cosmological_parameter_dict = get_cosmological_parameters()
     hubble_dist = cosmological_parameter_dict["D_H"]
-    hubble_const = cosmological_parameter_dict["H_0"]
+    h = cosmological_parameter_dict["h"]
     rest_frame_wl = cosmological_parameter_dict["HI rest frame wavelength"]
     omega_M = cosmological_parameter_dict["omega_M"]
     omega_k = cosmological_parameter_dict["omega_k"]
@@ -99,21 +100,20 @@ def get_kpar_conversion_factor(avg_freq_hz):
 
     avg_wl = c / avg_freq_hz
     z = avg_wl / rest_frame_wl - 1
-
-    # Line-of-sight conversion
+    rest_frame_freq = c / rest_frame_wl
     e_func = np.sqrt(omega_M * (1 + z) ** 3.0 + omega_k * (1 + z) ** 2.0 + omega_Lambda)
-    kpar_conv_factor = (2 * np.pi * hubble_const * e_func) / (
-        (1 + z) ** 2.0 * rest_frame_wl
-    )
+
+    kpar_conv_factor = (2 * np.pi * rest_frame_freq * e_func) / (hubble_dist * (1 + z)**2.)  # Units Mpc^-1
+    kpar_conv_factor /= h  # Units h/Mpc
+
     return kpar_conv_factor
 
 
 def get_kperp_conversion_factor(avg_freq_hz):
-    # See "Cosmological Parameters and Conversions Memo"
 
     cosmological_parameter_dict = get_cosmological_parameters()
     hubble_dist = cosmological_parameter_dict["D_H"]
-    hubble_const = cosmological_parameter_dict["H_0"]
+    h = cosmological_parameter_dict["h"]
     rest_frame_wl = cosmological_parameter_dict["HI rest frame wavelength"]
     omega_M = cosmological_parameter_dict["omega_M"]
     omega_k = cosmological_parameter_dict["omega_k"]
@@ -136,7 +136,9 @@ def get_kperp_conversion_factor(avg_freq_hz):
         ),
     )
     dist_comoving = hubble_dist * dist_comoving_int
-    kperp_conv_factor = 2 * np.pi / dist_comoving
+    kperp_conv_factor = 2 * np.pi / dist_comoving  # Units Mpc^-1
+    kperp_conv_factor /= h  # Units h/Mpc
+
     return kperp_conv_factor
 
 
@@ -161,11 +163,10 @@ def uvn_to_cosmology_axis_transform(
 
 def matter_ps_to_21cm_ps_conversion(
     k_axis,  # Units h/Mpc
-    matter_ps,  # Units (Mpc/h)^3
+    matter_ps,
     z,  # redshift
 ):
     # See Pober et al. 2013
-    # Produces a slight difference from the paper results. Why?
 
     cosmological_parameter_dict = get_cosmological_parameters()
     omega_M = cosmological_parameter_dict["omega_M"]
@@ -359,7 +360,6 @@ def get_sample_variance(
     corr_volume = (
         kpar_conv_factor * delay_corr_len * (kperp_conv_factor * uv_corr_len) ** 2.0
     )
-    print(f"correlation volume: {corr_volume}")
 
     # Calculate sampled volume
     sampling_volumes = np.zeros(len(k_bin_edges) - 1, dtype=float)
@@ -444,6 +444,6 @@ def get_sample_variance(
     nsamples = sampling_volumes / corr_volume
 
     ps_model_interp = np.interp(k_bin_centers, model_k_axis, ps_model)
-    sample_var = ps_model_interp / nsamples
+    sample_var = ps_model_interp**2.0 / nsamples
 
     return sample_var

run_calculate_sensitivity.py
@@ -4,7 +4,6 @@ import array_sensitivity
 
 
 # Set instrument parameters
-antpos_filepath = "20210226W.cfg"
 c = 3e8
 min_freq_hz = 0.7e9
 max_freq_hz = c / 0.21
@@ -33,233 +32,49 @@ max_kperp = kperp_conv_factor * max_baseline_wl
 max_k = np.sqrt(max_kpar**2.0 + max_kperp**2.0)
 
 # Define bin edges:
-k_bin_size = 0.2
-bin_edges = np.arange(0, max_k, k_bin_size)
+k_bin_size = 0.1
+min_k = 0.02
+bin_edges = np.arange(min_k, max_k, k_bin_size)
 kpar_bin_edges = np.arange(0, max_kpar, k_bin_size)
 kperp_bin_edges = np.arange(0, max_kperp, k_bin_size)
 
-if False:
-    # Calculate zenith-pointed thermal noise
-    (
-        nsamples,
-        binned_ps_variance,
-        true_bin_edges,
-        true_bin_centers,
-        nsamples_2d,
-        binned_ps_variance_2d,
-    ) = array_sensitivity.delay_ps_sensitivity_analysis(
-        antpos_filepath=antpos_filepath,
-        min_freq_hz=min_freq_hz,
-        max_freq_hz=max_freq_hz,
-        tsys_k=tsys_k,
-        aperture_efficiency=aperture_efficiency,
-        antenna_diameter_m=antenna_diameter_m,
-        freq_resolution_hz=freq_resolution_hz,
-        int_time_s=int_time_s,
-        max_bl_m=max_bl_m,
-        k_bin_edges_1d=bin_edges,
-        kpar_bin_edges=kpar_bin_edges,
-        kperp_bin_edges=kperp_bin_edges,
-        zenith_angle=0.0,
-    )
-    f = open("simulation_outputs/zenith_thermal_noise.npy", "wb")
-    np.save(f, nsamples)
-    np.save(f, binned_ps_variance)
-    np.save(f, true_bin_edges)
-    np.save(f, true_bin_centers)
-    np.save(f, nsamples_2d)
-    np.save(f, binned_ps_variance_2d)
-    f.close()
+antpos_filepaths = ["20210226W.cfg", "20210226W_core_100.cfg", "20210226W_core_200.cfg"]
+wedge_extents = [90.0, 1.8]
+pointing_angles = [0., 60.]
 
-    # Calculate off-zenith thermal noise
-    (
-        nsamples_offzenith,
-        binned_ps_variance_offzenith,
-        true_bin_edges_offzenith,
-        true_bin_centers_offzenith,
-        nsamples_2d_offzenith,
-        binned_ps_variance_2d_offzenith,
-    ) = array_sensitivity.delay_ps_sensitivity_analysis(
-        antpos_filepath=antpos_filepath,
-        min_freq_hz=min_freq_hz,
-        max_freq_hz=max_freq_hz,
-        tsys_k=tsys_k,
-        aperture_efficiency=aperture_efficiency,
-        antenna_diameter_m=antenna_diameter_m,
-        freq_resolution_hz=freq_resolution_hz,
-        int_time_s=int_time_s,
-        max_bl_m=max_bl_m,
-        k_bin_edges_1d=bin_edges,
-        kpar_bin_edges=kpar_bin_edges,
-        kperp_bin_edges=kperp_bin_edges,
-        zenith_angle=60.0,
-    )
-    f = open("simulation_outputs/off_zenith_thermal_noise.npy", "wb")
-    np.save(f, nsamples_offzenith)
-    np.save(f, binned_ps_variance_offzenith)
-    np.save(f, true_bin_edges_offzenith)
-    np.save(f, true_bin_centers_offzenith)
-    np.save(f, nsamples_2d_offzenith)
-    np.save(f, binned_ps_variance_2d_offzenith)
-    f.close()
-
-    # Calculate thermal noise with 200-antenna core
-    (
-        nsamples,
-        binned_ps_variance,
-        true_bin_edges,
-        true_bin_centers,
-        nsamples_2d,
-        binned_ps_variance_2d,
-    ) = array_sensitivity.delay_ps_sensitivity_analysis(
-        antpos_filepath="20210226W_core_200.cfg",
-        min_freq_hz=min_freq_hz,
-        max_freq_hz=max_freq_hz,
-        tsys_k=tsys_k,
-        aperture_efficiency=aperture_efficiency,
-        antenna_diameter_m=antenna_diameter_m,
-        freq_resolution_hz=freq_resolution_hz,
-        int_time_s=int_time_s,
-        max_bl_m=max_bl_m,
-        k_bin_edges_1d=bin_edges,
-        kpar_bin_edges=kpar_bin_edges,
-        kperp_bin_edges=kperp_bin_edges,
-        zenith_angle=0.0,
-    )
-    f = open("simulation_outputs/zenith_thermal_noise_core_200.npy", "wb")
-    np.save(f, nsamples)
-    np.save(f, binned_ps_variance)
-    np.save(f, true_bin_edges)
-    np.save(f, true_bin_centers)
-    np.save(f, nsamples_2d)
-    np.save(f, binned_ps_variance_2d)
-    f.close()
-
-    # Calculate off-zenith thermal noise with 200-antenna core
-    (
-        nsamples_offzenith,
-        binned_ps_variance_offzenith,
-        true_bin_edges_offzenith,
-        true_bin_centers_offzenith,
-        nsamples_2d_offzenith,
-        binned_ps_variance_2d_offzenith,
-    ) = array_sensitivity.delay_ps_sensitivity_analysis(
-        antpos_filepath="20210226W_core_200.cfg",
-        min_freq_hz=min_freq_hz,
-        max_freq_hz=max_freq_hz,
-        tsys_k=tsys_k,
-        aperture_efficiency=aperture_efficiency,
-        antenna_diameter_m=antenna_diameter_m,
-        freq_resolution_hz=freq_resolution_hz,
-        int_time_s=int_time_s,
-        max_bl_m=max_bl_m,
-        k_bin_edges_1d=bin_edges,
-        kpar_bin_edges=kpar_bin_edges,
-        kperp_bin_edges=kperp_bin_edges,
-        zenith_angle=60.0,
-    )
-    f = open("simulation_outputs/off_zenith_thermal_noise_core_200.npy", "wb")
-    np.save(f, nsamples_offzenith)
-    np.save(f, binned_ps_variance_offzenith)
-    np.save(f, true_bin_edges_offzenith)
-    np.save(f, true_bin_centers_offzenith)
-    np.save(f, nsamples_2d_offzenith)
-    np.save(f, binned_ps_variance_2d_offzenith)
-    f.close()
-
-    # Calculate thermal noise with 100-antenna core
-    (
-        nsamples,
-        binned_ps_variance,
-        true_bin_edges,
-        true_bin_centers,
-        nsamples_2d,
-        binned_ps_variance_2d,
-    ) = array_sensitivity.delay_ps_sensitivity_analysis(
-        antpos_filepath="20210226W_core_100.cfg",
-        min_freq_hz=min_freq_hz,
-        max_freq_hz=max_freq_hz,
-        tsys_k=tsys_k,
-        aperture_efficiency=aperture_efficiency,
-        antenna_diameter_m=antenna_diameter_m,
-        freq_resolution_hz=freq_resolution_hz,
-        int_time_s=int_time_s,
-        max_bl_m=max_bl_m,
-        k_bin_edges_1d=bin_edges,
-        kpar_bin_edges=kpar_bin_edges,
-        kperp_bin_edges=kperp_bin_edges,
-        zenith_angle=0.0,
-    )
-    f = open("simulation_outputs/zenith_thermal_noise_core_100.npy", "wb")
-    np.save(f, nsamples)
-    np.save(f, binned_ps_variance)
-    np.save(f, true_bin_edges)
-    np.save(f, true_bin_centers)
-    np.save(f, nsamples_2d)
-    np.save(f, binned_ps_variance_2d)
-    f.close()
-
-    # Calculate off-zenith thermal noise with 100-antenna core
-    (
-        nsamples_offzenith,
-        binned_ps_variance_offzenith,
-        true_bin_edges_offzenith,
-        true_bin_centers_offzenith,
-        nsamples_2d_offzenith,
-        binned_ps_variance_2d_offzenith,
-    ) = array_sensitivity.delay_ps_sensitivity_analysis(
-        antpos_filepath="20210226W_core_100.cfg",
-        min_freq_hz=min_freq_hz,
-        max_freq_hz=max_freq_hz,
-        tsys_k=tsys_k,
-        aperture_efficiency=aperture_efficiency,
-        antenna_diameter_m=antenna_diameter_m,
-        freq_resolution_hz=freq_resolution_hz,
-        int_time_s=int_time_s,
-        max_bl_m=max_bl_m,
-        k_bin_edges_1d=bin_edges,
-        kpar_bin_edges=kpar_bin_edges,
-        kperp_bin_edges=kperp_bin_edges,
-        zenith_angle=60.0,
-    )
-    f = open("simulation_outputs/off_zenith_thermal_noise_core_100.npy", "wb")
-    np.save(f, nsamples_offzenith)
-    np.save(f, binned_ps_variance_offzenith)
-    np.save(f, true_bin_edges_offzenith)
-    np.save(f, true_bin_centers_offzenith)
-    np.save(f, nsamples_2d_offzenith)
-    np.save(f, binned_ps_variance_2d_offzenith)
-    f.close()
-
-# Calculate zenith-pointed thermal noise with FoV wedge cut
-(
-    nsamples,
-    binned_ps_variance,
-    true_bin_edges,
-    true_bin_centers,
-    nsamples_2d,
-    binned_ps_variance_2d,
-) = array_sensitivity.delay_ps_sensitivity_analysis(
-    antpos_filepath=antpos_filepath,
-    min_freq_hz=min_freq_hz,
-    max_freq_hz=max_freq_hz,
-    tsys_k=tsys_k,
-    aperture_efficiency=aperture_efficiency,
-    antenna_diameter_m=antenna_diameter_m,
-    freq_resolution_hz=freq_resolution_hz,
-    int_time_s=int_time_s,
-    max_bl_m=max_bl_m,
-    k_bin_edges_1d=bin_edges,
-    kpar_bin_edges=kpar_bin_edges,
-    kperp_bin_edges=kperp_bin_edges,
-    wedge_extent_deg=1.84,
-    zenith_angle=0.0,
-)
-f = open("simulation_outputs/zenith_thermal_noise_fov_wedge.npy", "wb")
-np.save(f, nsamples)
-np.save(f, binned_ps_variance)
-np.save(f, true_bin_edges)
-np.save(f, true_bin_centers)
-np.save(f, nsamples_2d)
-np.save(f, binned_ps_variance_2d)
-f.close()
+for array_config_ind, antpos_filepath in antpos_filepaths:
+    ant_config_name = (["no_core", "core100", "core2000"])[array_config_ind]
+    for wedge_ext_ind, wedge_ext in wedge_extents:
+        wedge_cut_name = (["horizon", "fov"])[wedge_ext_ind]
+        for pointing_ang in pointing_angles:
+            (
+                nsamples,
+                binned_ps_variance,
+                true_bin_edges,
+                true_bin_centers,
+                nsamples_2d,
+                binned_ps_variance_2d,
+            ) = array_sensitivity.delay_ps_sensitivity_analysis(
+                antpos_filepath=antpos_filepath,
+                min_freq_hz=min_freq_hz,
+                max_freq_hz=max_freq_hz,
+                tsys_k=tsys_k,
+                aperture_efficiency=aperture_efficiency,
+                antenna_diameter_m=antenna_diameter_m,
+                freq_resolution_hz=freq_resolution_hz,
+                int_time_s=int_time_s,
+                max_bl_m=max_bl_m,
+                k_bin_edges_1d=bin_edges,
+                kpar_bin_edges=kpar_bin_edges,
+                kperp_bin_edges=kperp_bin_edges,
+                zenith_angle=pointing_ang,
+                wedge_extent_deg=wedge_ext,
+            )
+            f = open(f"simulation_outputs/hermal_noise_{ant_config_name}_wedge_cut_{wedge_cut_name}_za_{pointing_ang}.npy", "wb")
+            np.save(f, nsamples)
+            np.save(f, binned_ps_variance)
+            np.save(f, true_bin_edges)
+            np.save(f, true_bin_centers)
+            np.save(f, nsamples_2d)
+            np.save(f, binned_ps_variance_2d)
+            f.close()